7178ab614a078fe88bdc29db3e9afe51bfe8835e
Prince Sonani <psona2@uic.edu>
2024-12-02 19:01:26 -0600

    Add hello world service and update Docker configuration for multiple services

--- vulnerable-server/printenv.c
+++ vulnerable-server/printenv.c
@@ -2,8 +2,29 @@
 #include <stdio.h>
 #include <string.h>
 
-void main(){
-    char* shell = getenv("MYSHELL");
-    if (shell)
-        printf("%x\n", (unsigned int)shell);
+int main() {
+    // Open a file to write the addresses
+    FILE file = fopen("addresses.txt", "w");
+    if (!file) {
+        perror("Failed to open file");
+        return 1;
+    }
+
+    // Get and write the address of the MYSHELL environment variable
+    charshell = getenv("MYSHELL");
+    if (shell) {
+        fprintf(file, "%x\n", (unsigned int)shell);
+    } else {
+        fprintf(file, "MYSHELL environment variable is not set.\n");
+    }
+
+    // Get and write the address of the system() function
+    void (*sys_ptr)() = system; // Function pointer to system()
+    fprintf(file, "%x\n", (unsigned int)sys_ptr);
+
+    // Close the file
+    fclose(file);
+
+    printf("Addresses written to addresses.txt\n");
+    return 0;
 }
